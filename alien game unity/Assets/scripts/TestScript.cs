acb33f18a218a82e30b5729ac3610af4ecdc1936
IbbyXX <ibby09k@gmail.com>
2024-11-20 10:49:09 +0000

    added image and text and made image move

---
+++ alien game unity/Assets/scripts/TestScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //adds text to console
+        Debug.Log("hello world");
+        Debug.Log("hello worl");
+        Debug.Log("hello wor");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //spams text in console
+        Debug.Log("hello");
+
+        //smoothly moves image
+        transform.Translate(0.0005f, 0, 0);
+    }
+}
